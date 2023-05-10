da34a89a466611a1e2f5c2670ddb69863db4d331
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-10 09:24:16 -0400

    initial commmit

---
+++ src/main/java/sheridan/tetervak/myhellouser/MyHelloUserApplication.java
@@ -0,0 +1,13 @@
+package sheridan.tetervak.myhellouser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHelloUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHelloUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/tetervak/myhellouser/MyHelloUserApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.tetervak.myhellouser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHelloUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
